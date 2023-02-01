07fe91c0029ca584eea7c9b421a7b1e7fe224540
TrajanP <77470574+TrajanP@users.noreply.github.com>
2023-01-31 20:46:25 -0600

    Create index.js

---
+++ WebDev2/index.js
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="index.css"/>
+    <link rel="shortcut icon" href="masterchief_icon.ico"/>
+    <!-- <link rel="icon" type="image/png" href="masterchief_icon.png" /> -->
+
+    <title>Trajan WebDev-2</title>
+    <!-- <meta name="description" content="Our first page">
+    <meta name="keywords" content="html tutorial template"> -->
+</head>
+<body>
+    <div class="NavBar">
+        <h1>Trajan's Web Dev 2 Homepage</h1>
+    </div>
+    <div class="list">
+        <ul> 
+            <h3>
+                <li><a href="Bootstrap/indexBS.html">Bootstrap Webpage</a></li>
+                <li> Chapter 1 </li>
+                    <ul>
+                        <li><a href="WebDev/Ch1/BookORama/index1.html">Book O'Rama Webpage v1</a></li>
+                    </ul>
+                <li> Chapter 2 </li>
+                    <ul>
+                        <li><a href="WebDev2/Ch2/BookORama/index1.html">Book O'Rama Webpage v2</li>
+                        <li><a href="WebDev2/Ch2/Exercise2-1.html">Exercise 2-1</li>
+                        <li><a href="WebDev2/Ch2/Exercise2-2.html">Exercise 2-2</li>
+                    </ul>
+                <li> Chapter 3 </li>
+                    <ul>
+                        <li><a href="WebDev2/Ch3/BookORama/index1.html">Book O'Rama Webpage v3</li>
+                        <li><a href="WebDev2/Ch3/Exercise3-1/index.html">Exercise 3-1</li>
+                        <li><a href="WebDev2/Ch3/Exercise3-2/index.html">Exercise 3-2</li>
+                    </ul>
+            </h3>
+        </ul>
+
+    </div>
+</body>
+</html>
