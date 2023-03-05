14b8b4badec3e1ae5818a720965bc71044284135
TrajanP <77470574+TrajanP@users.noreply.github.com>
2023-03-04 19:05:50 -0600

    Update portfolio.js

--- WebDev2/FinalProject/JS/portfolio.js
+++ WebDev2/FinalProject/JS/portfolio.js
@@ -1,95 +1,18 @@
-// const slideImage = document.querySelectorAll(".slide-image");
-// const slidesContainer = document.querySelector(".slides-container");
-// const nextBtn = document.querySelector(".next-btn");
-// const prevBtn = document.querySelector(".prev-btn");
-// const navigationDots = document.querySelector(".navigation-dots");
-
-// let numberOfImages = slideImage.length;
-// let slideWidth = slideImage[0].clientWidth;
-// let currentSlide = 0;
-
-// // Set up the slider
-
-// function init() {
-    
-//   slideImage.forEach((img, i) => {
-//     img.style.left = i * 100 + "%";
-//   });
-
-//   slideImage[0].classList.add("active");
-
-//   createNavigationDots();
-// }
-
-// init();
-
-// // Create navigation dots
-
-// function createNavigationDots() {
-//   for (let i = 0; i < numberOfImages; i++) {
-//     const dot = document.createElement("div");
-//     dot.classList.add("single-dot");
-//     navigationDots.appendChild(dot);
-
-//     dot.addEventListener("click", () => {
-//       goToSlide(i);
-//     });
-//   }
-
-//   navigationDots.children[0].classList.add("active");
-// }
-
-// // Next Button
-
-// nextBtn.addEventListener("click", () => {
-//   if (currentSlide >= numberOfImages - 1) {
-//     goToSlide(0);
-//     return;
-//   }
-
-//   currentSlide++;
-//   goToSlide(currentSlide);
-// });
-
-// // Previous Button
-
-// prevBtn.addEventListener("click", () => {
-//   if (currentSlide <= 0) {
-//     goToSlide(numberOfImages - 1);
-//     return;
-//   }
-
-//   currentSlide--;
-//   goToSlide(currentSlide);
-// });
-
-// // Go To Slide
-
-// function goToSlide(slideNumber) {
-//   slidesContainer.style.transform =
-//     "translateX(-" + slideWidth * slideNumber + "px)";
-
-//   currentSlide = slideNumber;
-
-//   setActiveClass();
-// }
-
-// // Set Active Class
-
-// function setActiveClass() {
-//   //Set active class for Slide Image
-
-//   let currentActive = document.querySelector(".slide-image.active");
-//   currentActive.classList.remove("active");
-//   slideImage[currentSlide].classList.add("active");
-
-//   //Set active class for navigation dots
-//   let currentDot = document.querySelector(".single-dot.active");
-//   currentDot.classList.remove("active");
-//   navigationDots.children[currentSlide].classList.add("active");
-// }
-
-
+//Preload Images
+if (document.images) {
+  img1 = new Image();
+  img1.src = "../Media/bisonbits1.png";
+  img2 = new Image();
+  img2.src = "../Media/bisonbits2.png";
+  img3 = new Image();
+  img3.src = "../Media/bisonbits3.png";
+  img4 = new Image();
+  img4.src = "../Media/bisonbits4.png";
+  img5 = new Image();
+  img5.src = "../Media/bisonbits5.png";
+  img6 = new Image();
+  img6.src = "../Media/bisonbits6.png";
+}
 
 //This is a horrible way to hard code each slides functionality...
 //should've designed it generic functions that get passed a slides attributes
